86842c1cb40d32a06a74d3e16f284d9acd9f4932
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-05-08 18:22:37 -0400

    centered emoji

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Emojibox from './Components/emojibox'
 import emojiList from './emojis'
+import RefreshButton from './Components/refreshbutton'
 
 function App() {
   const emoji = require("./emojis.js");
   var random = Math.floor(Math.random() * emojiList.length);
   var selectedEmoji = emojiList[random].text;
   return (
-    <div id = "container">
+    <div class = "container">
           <Emojibox id = "emoji" text={selectedEmoji}></Emojibox>
+          <RefreshButton></RefreshButton>
     </div>
   );
 }
---
+++ my-app/src/Components/refreshbutton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RefreshButton extends Component {
+    render() {
+        return (
+            <button type="button">
+                Refresh
+            </button>
+        )
+    }
+}
